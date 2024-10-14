4f8eb86b74d493da4dd17deb83bf8ec7b77dd673
adji142 <prasetyoajiw@gmail.com>
2024-10-14 15:20:45 +0700

    test register 7

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -244,7 +244,27 @@ License: You must have a valid license purchased only from themeforest(the above
                     </div>
                  
                 </div>
-
+    
+                <div class="form-group  row">
+                    <div class="col-lg-2 col-3 ">
+                        <label for="exampleInputEmail1" class="mb-0 text-dark">
+                            <svg xmlns="http://www.w3.org/2000/svg"  width="20px" height="20px" fill="currentColor" class="bi bi-globe-americas" viewBox="0 0 16 16">
+                                <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0M2.04 4.326c.325 1.329 2.532 2.54 3.717 3.19.48.263.793.434.743.484q-.121.12-.242.234c-.416.396-.787.749-.758 1.266.035.634.618.824 1.214 1.017.577.188 1.168.38 1.286.983.082.417-.075.988-.22 1.52-.215.782-.406 1.48.22 1.48 1.5-.5 3.798-3.186 4-5 .138-1.243-2-2-3.5-2.5-.478-.16-.755.081-.99.284-.172.15-.322.279-.51.216-.445-.148-2.5-2-1.5-2.5.78-.39.952-.171 1.227.182.078.099.163.208.273.318.609.304.662-.132.723-.633.039-.322.081-.671.277-.867.434-.434 1.265-.791 2.028-1.12.712-.306 1.365-.587 1.579-.88A7 7 0 1 1 2.04 4.327Z"/>
+                            </svg>
+                        </label>
+                    </div>
+                        <div class="col-lg-10 col-9 pl-0">
+                        <select required id="ProvID" name="ProvID" class="js-example-basic-single form-control text-dark border-0 p-0 h-20px font-size-h5">
+                            <option value="">Pilih Provinsi</option>
+                            @foreach($provinsi as $ko)
+                                <option value="{{ $ko->prov_id }}">
+                                    {{ $ko->prov_name }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
+                 
+                </div>
     
                 <div class="form-group  row">
                     <div class="col-lg-2 col-3 ">
@@ -309,6 +329,38 @@ License: You must have a valid license purchased only from themeforest(the above
                     </div>
                  
                 </div>
+
+                <div class="form-group  row">
+                    <center>
+                        <label  class="text-body">Pilih Paket Berlangganan</label>
+                    </center>
+                    <div class="product-card-container">
+
+                        @foreach ($subscriptionheader as $item)
+                        <div class="product-card" attr-productselected="{{ $item['NoTransaksi'] }}" attr-productprice="{{ $item['Harga'] - $item['Potongan'] }}">
+                            <img src="{{ asset('images/custom/subscription.png') }}" alt="Product 1">
+                            <div class="product-details">
+                                <center>
+                                    <h5>{{ $item['NamaSubscription'] }}</h5>
+                                </center>
+                                @php
+                                    echo "<div class='deskripsi'>".$item['DeskripsiSubscription']."</div>";
+                                @endphp
+                                <center>
+                                    @if ($item['Potongan'] > 0)
+                                        <div class='original-price'>{{ number_format($item['Harga']) }}</div>
+                                        <div class='discount-price'>{{ number_format(($item['Harga'] - $item['Potongan'])) }}</div>
+                                    @else
+                                        <div class='price'>{{ number_format($item['Harga']) }}</div>
+                                    @endif
+                                    
+                                </center>
+                            </div>
+                        </div>
+                        @endforeach
+                        <!-- Add more product cards as needed -->
+                    </div>
+                </div>
     
                 <div class="form-group row ">
                     <div class="col-lg-2 col-3 ">
@@ -359,116 +411,5 @@ License: You must have a valid license purchased only from themeforest(the above
 <script src="{{asset('api/select2/select2.min.js')}}"></script>
 </body>
 <!--end::Body-->
-<script>
-    var oProvinsi;
-    var oKota;
-    var oKelurahan;
-    var oKecamatan;
-
-    var ProductSelected = "";
-    var ProductPrice = 0;
-
-	jQuery(document).ready(function() {
-        jQuery('.js-example-basic-single').select2();
-
-        oProvinsi = <?php echo $provinsi; ?>;
-        oKota = <?php echo $kota; ?>;
-        oKelurahan = <?php echo $kelurahan; ?>;
-        oKecamatan = <?php echo $kecamatan; ?>;
-    });
-
-    jQuery('#ProvID').change(function () {
-        const filterKota = oKota.filter(kota => kota.prov_id == jQuery('#ProvID').val());
-        $('#KotaID').empty();
-        var newOption = $('<option>', {
-            value: -1,
-            text: "Pilih Kota"
-        });
-        $('#KotaID').append(newOption);
-
-        $.each(filterKota,function (k,v) {
-            var newOption = $('<option>', {
-                value: v.city_id,
-                text: v.city_name
-            });
-
-            $('#KotaID').append(newOption);
-        });
-    });
-
-    jQuery('#KotaID').change(function () {
-        const filterkec = oKecamatan.filter(kec => kec.kota_id == jQuery('#KotaID').val());
-
-        $('#KecID').empty();
-        var newOption = $('<option>', {
-            value: -1,
-            text: "Pilih Kecamatan"
-        });
-        $('#KecID').append(newOption); 
-        $.each(filterkec,function (k,v) {
-            var newOption = $('<option>', {
-                value: v.dis_id,
-                text: v.dis_name
-            });
-
-            $('#KecID').append(newOption);
-        });
-    });
-
-    jQuery('#KecID').change(function () {
-        const filterkel = oKelurahan.filter(kel => kel.kec_id == jQuery('#KecID').val());
-
-        $('#KelID').empty();
-        var newOption = $('<option>', {
-            value: -1,
-            text: "Pilih Kelurahan"
-        });
-        $('#KelID').append(newOption); 
-        $.each(filterkel,function (k,v) {
-            var newOption = $('<option>', {
-                value: v.subdis_id,
-                text: v.subdis_name
-            });
-
-            $('#KelID').append(newOption);
-        });
-    });
-
-    jQuery('.product-card').click(function() {
-        jQuery('.product-card').removeClass('clicked');
-        jQuery(this).addClass('clicked');
-
-        ProductSelected = jQuery('.product-card').attr("attr-productselected");
-        ProductPrice = jQuery('.product-card').attr("attr-productprice");
-    });
-
-    jQuery('#DaftarLangganan').submit(function (event) {
-        event.preventDefault();
-        var form = $(this);
-        var formData = form.serializeArray();
-        formData.push({ name: 'ProductSelected', value: ProductSelected });
-        $.post(form.attr('action'), formData, function(response) {
-            // Handle the server response here
-            console.log(response);
-            Swal.fire({
-                html: "Data Langganan Berhasil disimpan, Silahkan Menghubungi Administrator untuk konfirmasi",
-                icon: "success",
-                title: "Horray...",
-                // text: "Data berhasil disimpan! <br> " + response.Kembalian,
-            }).then((result)=>{
-                window.location.href = '{{url("/")}}';
-            });
-        }).fail(function(jqXHR, textStatus, errorThrown) {
-            // Handle any errors here
-            console.error("Submission failed: ", textStatus, errorThrown);
-            Swal.fire({
-                html: "Gagal Melakukan registrasi : ",
-                icon: "error",
-                title: "Horray...",
-                // text: "Data berhasil disimpan! <br> " + response.Kembalian,
-            });
-        });
-    });
-</script>
 
 </html>
