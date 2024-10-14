eca7dbb8db80e854fed718f6021744a00607676d
adji142 <prasetyoajiw@gmail.com>
2024-10-14 16:15:20 +0700

    test register 15

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -424,10 +424,7 @@ License: You must have a valid license purchased only from themeforest(the above
         jQuery(document).ready(function() {
             jQuery('.js-example-basic-single').select2();
 
-            oProvinsi = <?php echo $provinsi; ?>;
-            oKota = <?php echo $kota; ?>;
-            oKelurahan = <?php echo $kelurahan; ?>;
-            oKecamatan = <?php echo $kecamatan; ?>;
+            
         });
 
         jQuery('#ProvID').change(function () {
